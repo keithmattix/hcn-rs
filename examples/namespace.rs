905d060b93c30e524c14b3b84b7c8c357daa5683
James Sturtevant <jstur@microsoft.com>
2023-09-12 11:31:40 -0700

    Does not work in WS versions
    Signed-off-by: James Sturtevant <jstur@microsoft.com>

--- examples/namespace.rs
+++ examples/namespace.rs
@@ -5,8 +5,10 @@ use windows::core::GUID;
 pub fn main() -> Result<(), Box<dyn std::error::Error>> {
     //turn logging on with $env:RUST_LOG="debug"      
     let _ = env_logger::try_init();
-    let mut api_namespace = HostComputeNamespace::default();
-    api_namespace.create_with_compartment = Some(true);
+    let api_namespace = HostComputeNamespace::default();
+
+    // this only works on latest versions of WS (WS vnext or windows 11 not 2019 or 2022)
+    //api_namespace.create_with_compartment = Some(true);
 
     // create a network with API
     let api_namespace = serde_json::to_string(&api_namespace).unwrap();
@@ -28,8 +30,10 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // Values should be the same as if we used the API as we did above
     assert_eq!(api_namespace.id, namespace.id);
-    assert!(namespace.namespace_id.is_some());
-    assert_eq!(api_namespace.namespace_id, namespace.namespace_id);
+    
+    // gets compartment if create_with_compartment
+    // assert!(namespace.namespace_id.is_some());
+    // assert_eq!(api_namespace.namespace_id, namespace.namespace_id);
 
     println!("Deleting network: {}", namespace.id);
     api::delete_namespace(&GUID::from(namespace.id.as_str()))?;
